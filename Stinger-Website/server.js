4632d64ee57d5cf804913d65bb09cf938f33b5fa
Henryforeman1 <79278940+Henryforeman1@users.noreply.github.com>
2021-11-27 00:06:09 -0500

    Added an dconnected a database through mongo db compass

--- Stinger-Website/server.js
+++ Stinger-Website/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+var MongoClient = require('mongodb').MongoClient
+var client= new MongoClient ("mongodb+srv://Student:CorgisAreDope@cluster0.h6c8l.mongodb.net/test?authSource=admin&replicaSet=atlas-13gy8k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
+client.connect();
+var DB = client.db("myProject");
+/*MongoClient.connect("mongodb+srv://Student:CorgisAreDope@cluster0.h6c8l.mongodb.net/test?authSource=admin&replicaSet=atlas-13gy8k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", function (err, client) {
+  if (err) throw err
+
+  var db = client.db('myFirstDatabase')
+
+  db.collection('students').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  })
+})*/
 
 const indexRoutes = require('./website/routes/index');
 
@@ -28,8 +43,14 @@ app.get('/profile', (req, res) => {
     res.send('./server.js/profile.html', { root: __dirname });
 });
 
-app.post('/login', urlencodedParser, function(req, res){
+app.post('/login', urlencodedParser, async function(req, res){
     console.log(req.body);
+    var collection = DB.collection ("users");
+    var users = (await collection.find({username:req.body.username}).toArray())
+    if users.length === 0{
+        res.send('./server.js/login.html', { root: __dirname });
+    }
+
 });
 
 app.post('/signup', urlencodedParser, function(req,res){
