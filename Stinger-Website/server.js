212bfe583e155ad047a17f5a2701257fda474779
AlphonseVotaIV <70405949+AlphonseVotaIV@users.noreply.github.com>
2021-11-30 23:58:17 -0500

    HTML Change, Navigation Bar Routed, and Progress Synced
    Temporary fix until after class on the 1st for the presentation.

--- Stinger-Website/server.js
+++ Stinger-Website/server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+//Sets up the post commands
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+//EJS setup
+//app.set('view engine', 'ejs');
+
 var MongoClient = require('mongodb').MongoClient
 var client= new MongoClient ("mongodb+srv://Student:CorgisAreDope@cluster0.h6c8l.mongodb.net/test?authSource=admin&replicaSet=atlas-13gy8k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
 client.connect();
 var DB = client.db("myProject");
+
 /*MongoClient.connect("mongodb+srv://Student:CorgisAreDope@cluster0.h6c8l.mongodb.net/test?authSource=admin&replicaSet=atlas-13gy8k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", function (err, client) {
   if (err) throw err
 
@@ -16,32 +26,7 @@ var DB = client.db("myProject");
 
     console.log(result)
   })
-})*/
-
-const indexRoutes = require('./website/routes/index');
-
-app.use(indexRoutes);
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-app.set('view engine', 'ejs');
-
-app.use(express.static('website'));
-
-app.get('/', (req, res) => {
-    res.send('./views/index', { root: __dirname });
-});
-
-app.get('/login', (req, res) => {
-    res.send('./server.js/login.html', { root: __dirname });
-});
-
-app.get('/profile', (req, res) => {
-    res.send('./server.js/profile.html', { root: __dirname });
-});
+})
 
 app.post('/login', urlencodedParser, async function(req, res){
     console.log(req.body);
@@ -51,12 +36,68 @@ app.post('/login', urlencodedParser, async function(req, res){
         res.send('./server.js/login.html', { root: __dirname });
     }
 
+});*/
+
+app.use(express.static('website'));
+
+//Sending the files to load on the localhost
+
+app.get('/', (req, res) => {
+    res.send('./website/index.html', { root: __dirname });
+});
+
+app.get('/', (req, res) => {
+    res.render('./website/index.html', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+    res.send('./server.js/contactus.html', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('./server.js/contactus.html', { root: __dirname });
+});
+
+app.get("/login", function (req, res) {
+    res.send('./server.js/login.html', { root: __dirname });
+})
+
+app.get("/login", function (req, res) {
+    res.render('./server.js/login.html', { root: __dirname });
+})
+
+app.get('/profile', (req, res) => {
+    res.send('./server.js/profile1.html', { root: __dirname });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('./server.js/profile1.html', { root: __dirname });
+});
+
+app.get('/signup', (req, res) => {
+    res.send('./server.js/signup.html', { root: __dirname });
+});
+
+app.get('/signup', (req, res) => {
+    res.render('./server.js/signup.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    res.send('./server.js/about.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    res.render('./server.js/about.html', { root: __dirname });
+});
+
+/*
+app.post('/login', urlencodedParser, function(req, res){
+    console.log(req.body);
 });
 
 app.post('/signup', urlencodedParser, function(req,res){
     console.log(req.body);
-});
-
+});*/
 
 app.get('/', function(req, res) {
     res.render("server.js");
