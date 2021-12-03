835dc956bb49244603dfb88e44d7d79fbd7c8f65
AlphonseVotaIV <70405949+AlphonseVotaIV@users.noreply.github.com>
2021-12-03 09:57:12 -0500

    Added 404 and 500 error messages

--- Stinger-Website/website/server.js
+++ Stinger-Website/website/server.js
@@ -105,4 +105,13 @@ app.post('/signup', urlencodedParser, function(req,res){
     console.log(req.body);
 });
 
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+/*app.use(function (req, res, next) {
+    res.status(404).send("404 Not Found")
+})*/
+
 app.listen(port, () => console.log(`listening on port ${port}!`));
