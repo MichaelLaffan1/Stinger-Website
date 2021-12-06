e707fc311b645eb9a5759b97d228ba9864743fb9
AlphonseVotaIV <70405949+AlphonseVotaIV@users.noreply.github.com>
2021-12-05 21:37:01 -0500

    Uncommented 404

--- Stinger-Website/website/server.js
+++ Stinger-Website/website/server.js
@@ -114,8 +114,8 @@ app.use(function (err, req, res, next) {
     res.status(500).send('Something broke!')
   })
 
-/*app.use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.status(404).send("404 Not Found")
-})*/
+})
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
